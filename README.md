e48c3d01e0c961611ec9f67edea46341081dd35c
Joshua Kruzan <joshua_kruzan@brown.edu>
2022-11-30 10:05:15 -0500

    Function to load CEDAR dataset

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # GAN-DLD
 Planning to make a `preprocess.py` in this outer directory and then a folder for each other model?
 
-You can download the dataset [here](https://cedar.buffalo.edu/NIJ/data/) and place it in a `/data` directory.
+You can download the english dataset [here](https://cedar.buffalo.edu/NIJ/data/) and place it in a `/data` directory.
 
-So far, I've had to 
-- `pip install unrar`
+I needed to download an app to extract the files from `.rar` file. I used [The Unarchiver](https://apps.apple.com/us/app/the-unarchiver/id425424353?mt=12).
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,41 @@
-from unrar import rarfile
+import glob
+import numpy as np
+from PIL import Image
 
-path = './data/signatures.rar'
-path = rarfile.RarFile()
+def get_CEDAR():
+    """
+    Loads CEDAR dataset. The dataset needs to be extracted from .rar file first
+
+    :return X0: training images,
+            Y0: training labels,
+            X1: testing images,
+            Y1: testing labels
+    """
+    org_paths = './data/signatures/full_org/*.png'
+    forg_paths = './data/signatures/full_forg/*.png'
+
+    D = []
+    # Load original signatures
+    print("Loading CEDAR original images")
+    for im_path in glob.glob(org_paths):
+        image = Image.open(im_path)
+        im_data = np.asarray(image)
+        D.append((im_data, 0))
+
+    # Load forged signatures
+    print("Loading CEDAR forged signatures")
+    for im_path in glob.glob(forg_paths):
+        image = Image.open(im_path)
+        im_data = np.asarray(image)
+        D.append((im_data, 1))
+    
+    # Shuffle dataset
+    rng = np.random.default_rng()
+    rng.shuffle(D)
+
+    split = int(len(D)*0.8)
+    train = D[:split]
+    test = D[split:]
+    X0, Y0 = zip(*train)
+    X1, Y1 = zip(*test)
+    return X0, Y0, X1, Y1
