c323c7eaf881e21eed1b79e14d21b5cfcf199032
Joshua Kruzan <joshua_kruzan@brown.edu>
2022-12-01 18:17:45 -0500

    Update siamese

--- models/cnn.py
+++ models/cnn.py
@@ -2,29 +2,28 @@ import numpy as np
 import tensorflow as tf
 from utils import euclidean_distance
 
-def make_siamese_model(embed_size=48):
-    inputs = tf.keras.layers.Input((64,128,1))
-    x = tf.keras.layers.Conv2D(64, (2,2), padding='same', activation='relu')(inputs)
-    x = tf.keras.layers.MaxPooling2D(pool_size=(2,2))(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-
-
-    x = tf.keras.layers.Conv2D(64, (2, 2), padding="same", activation="relu")(x)
-    x = tf.keras.layers.MaxPooling2D(pool_size=2)(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
-
-    pooled_out = tf.keras.layers.GlobalAveragePooling2D()(x)
-    outputs = tf.keras.layers.Dense(embed_size)(pooled_out)
-
-    model = tf.keras.models.Model(inputs, outputs)
-    return model
+def get_cnn_model(embed_size=48):
+    cnn = tf.keras.Sequential([
+        # First convolution layers
+        tf.keras.layers.Conv2D(64, (2,2), padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+        tf.keras.layers.Dropout(0.2),
+        # Second convolution layer
+        tf.keras.layers.Conv2D(64, (2, 2), padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        tf.keras.layers.Dropout(0.3),
+        # Output embeddings
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(embed_size)
+    ])
+    return cnn
 
 def get_siamese_model(embed_size=48):
     im_a = tf.keras.layers.Input(shape=(64,128,1))
     im_b = tf.keras.layers.Input(shape=(64,128,1))
-    feature_extractor = make_siamese_model(embed_size)
-    feats_a = feature_extractor(im_a)
-    feats_b = feature_extractor(im_b)
+    cnn = get_cnn_model(embed_size)
+    feats_a = cnn(im_a)
+    feats_b = cnn(im_b)
     distance = tf.keras.layers.Lambda(euclidean_distance)([feats_a, feats_b])
     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(distance)
     model = tf.keras.models.Model(inputs=[im_a, im_b], outputs=outputs)
