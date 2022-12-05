62fe36700e946048003645e7bc316ebe6c9869d0
Joshua Kruzan <joshua_kruzan@brown.edu>
2022-12-05 15:50:17 -0500

    First experiment with results

--- experiments.py
+++ experiments.py
@@ -15,12 +15,13 @@ TODO: Lookup best practice for how to train with new samples in this case. Just 
     - See how just training with new data effects old accuracy
 
 3. Which model learns quicker? (English added to english model or english added to all model)
-        
-
 4. If the above can be accomplished, can the model learn with no forged examples? (Just label 0 pairs)
 5. Can we induce bias to ensure forgeries don't get past?
+
 NOTE: All experiments run with random SEED=42
 TODO: How would a system like this actually be used?
+ - in a real setting, probably want a general purpose forgery detector
+ - this model probably just learns a few individual's signatures and learns to detect them
 NOTE: Transformer trained with al in about 12 epochs for sure 15
 
 """
@@ -28,6 +29,9 @@ from models.transformer import get_transformer_model
 from models.cnn import get_siamese_model
 from preprocess import *
 from experiment_helpers import *
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
 def experiment_one(task):
     if task in ['1', 'all']:
         print("*************TASK 1*****************")
@@ -40,6 +44,57 @@ def experiment_one(task):
         for dataset in ['all']:
             X0, Y0, _, _ = get_filtered_dataset(dataset)
             train_models(X0, Y0, dataset, filtered=True)
+    if task in ['3', 'all']:
+        print('*************TASK 3*****************')
+        for dataset in ['cedar', 'bengali', 'hindi', 'all']:
+            f = open('experiment1.txt', 'a')
+            f.write("Accuracy on models with seen signatures: \n".upper())
+            f.close()
+            # Test models trained on full dataset
+            _, _, X1, Y1 = get_full_dataset(dataset)
+            cnn = get_siamese_model()
+            cnn.load_weights('./cnn/full_'+dataset)
+
+            transformer = get_transformer_model()
+            transformer.load_weights('./transformer/full_'+dataset)
+
+            test_model(X1, Y1, cnn, dataset)
+            test_model(X1, Y1, transformer, dataset, False)
+            # Test models on held out users
+            _, _, X1, Y1 = get_filtered_dataset(dataset)
+            cnn = get_siamese_model()
+            cnn.load_weights('./cnn/filtered_'+dataset)
+
+            transformer = get_transformer_model()
+            transformer.load_weights('./transformer/filtered_'+dataset)
+            test_model(X1, Y1, cnn, dataset)
+            test_model(X1, Y1, transformer, dataset, False)
+
+def test_model(X1, Y1, model, dataset, isCnn=True):
+    name = 'CNN' if isCnn else 'Transformer'
+    y_pred = tf.round(model.predict([X1[:,0], X1[:,1]]))
+    y_pred = tf.reshape(y_pred, (-1,1))
+    confusion = confusion_matrix(Y1, y_pred)
+    tn = confusion[0,0]
+    tp = confusion[1,1]
+    fn = confusion[1,0]
+    fp = confusion[0,1]
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    accuracy = (tp+tn)/(tp+fp+fn+tn)
+    f = open('experiment1.txt', 'a') 
+    f.write("Model: " + name +  "  Dataset: " + dataset.upper() + '\n')
+    print("Model: ", name, "  Dataset: ", dataset.upper())
+    f.write('Accuracy: ' + str(accuracy) + '\n')
+    f.write('Precision: ' + str(precision) + '\n')
+    f.write('Recall: ' + str(recall) + '\n')
+    print('Accuracy: ', str(accuracy))
+    f.write('Confusion Matrix: \n')
+    f.write(str(confusion))
+    f.write("\n************************************************************** \n")
+    f.close()
+    print('Confusion Matrix: \n')
+    print(str(confusion))
 
 def train_models(X0,Y0,dataset, filtered=False):
     name = 'filtered' if filtered else 'full'
